e691a307ea1d3a15ffcced6259a7316b307d0a8a
madiyar8610 <madiyar8610@mail.ru>
2023-09-25 16:15:01 +0500

    add Example005_ifelse

---
+++ Example005_ifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Login: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Ura, etoje MASHA !");
+}
+else
+{
+    Console.Write("Privet, ");
+    Console.WriteLine(username);
+}
