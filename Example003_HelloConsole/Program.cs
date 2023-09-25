3e72be12f07bb250d414a8e990fd1ed099e0a4fb
madiyar8610 <madiyar8610@mail.ru>
2023-09-25 09:50:07 +0500

    add Example004_Div

--- Example003_HelloConsole/Program.cs
+++ Example003_HelloConsole/Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int namberA = 3;
+int namberB = 5;
+int result = namberA + namberB;
+Console.WriteLine(result);
+
