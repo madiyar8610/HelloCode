5558564bd7b9e4889f07d0d1811cd5b2aef06c24
madiyar8610 <madiyar8610@mail.ru>
2023-09-25 10:13:34 +0500

    add Random

--- Example003_HelloConsole/Program.cs
+++ Example003_HelloConsole/Program.cs
@@ -1,5 +1,8 @@
-﻿int namberA = 3;
-int namberB = 5;
+﻿int namberA = new Random().Next(1,10); // 1 2 3 4 5 6 7 8 9
+Console.WriteLine(namberA);
+int namberB = new Random().Next(1,10);
+Console.WriteLine(namberB);
 int result = namberA + namberB;
 Console.WriteLine(result);
+
 
